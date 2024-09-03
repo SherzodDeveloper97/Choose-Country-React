87148578ee3ca4890e052474d3f3bec7b34382da
Sherzod Aliyev <111228212+SherzodDeveloper97@users.noreply.github.com>
2024-09-03 11:11:06 +0500

    first-commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ChooseCountry from './Components/ChooseCountry';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ChooseCountry />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ChooseCountry.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './choose-country.css';
+
+function ChooseCountry(){
+    const [country, setCountry] = useState("Tashkent");
+
+    const changeCountry = (event) => {
+        // console.log(event.target.classList.contains("btn-primary"))
+        if(event.target.classList.contains("btn-primary")){
+            setCountry("Tashkent")
+        }else if(event.target.classList.contains("btn-warning")){
+            setCountry("Washington")
+        }else if(event.target.classList.contains("btn-success")){
+            setCountry("Warsaw")
+        }
+    }
+    
+    return(
+        <div className='container'>
+            <h1 className="title">Choose your country</h1>
+            <hr />
+            <div className='select-country'>
+                <button className='btn btn-primary' onClick={changeCountry}>Uzbekistan</button>
+                <button className='btn btn-warning' onClick={changeCountry}>USA</button>
+                <button className='btn btn-success' onClick={changeCountry}>Poland</button>
+            </div>
+            <hr />
+            <p>{country}</p>
+        </div>
+    )
+}
+
+export default ChooseCountry;
