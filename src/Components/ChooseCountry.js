a906e918991fc2328a6450bbcb023dceedccb319
Sherzod Aliyev <111228212+SherzodDeveloper97@users.noreply.github.com>
2024-09-03 11:19:59 +0500

    Update ChooseCountry.js

--- src/Components/ChooseCountry.js
+++ src/Components/ChooseCountry.js
@@ -4,25 +4,25 @@ import './choose-country.css';
 function ChooseCountry(){
     const [country, setCountry] = useState("Tashkent");
 
-    const changeCountry = (event) => {
-        // console.log(event.target.classList.contains("btn-primary"))
-        if(event.target.classList.contains("btn-primary")){
-            setCountry("Tashkent")
-        }else if(event.target.classList.contains("btn-warning")){
-            setCountry("Washington")
-        }else if(event.target.classList.contains("btn-success")){
-            setCountry("Warsaw")
-        }
-    }
+    // const changeCountry = (event) => {
+    //     console.log(event.target.classList.contains("btn-primary"))
+    //     if(event.target.classList.contains("btn-primary")){
+    //         setCountry("Tashkent")
+    //     }else if(event.target.classList.contains("btn-warning")){
+    //         setCountry("Washington")
+    //     }else if(event.target.classList.contains("btn-success")){
+    //         setCountry("Warsaw")
+    //     }
+    // }
     
     return(
         <div className='container'>
             <h1 className="title">Choose your country</h1>
             <hr />
             <div className='select-country'>
-                <button className='btn btn-primary' onClick={changeCountry}>Uzbekistan</button>
-                <button className='btn btn-warning' onClick={changeCountry}>USA</button>
-                <button className='btn btn-success' onClick={changeCountry}>Poland</button>
+                <button className='btn btn-primary' onClick={() => {setCountry("Tashkent")}}>Uzbekistan</button>
+                <button className='btn btn-warning' onClick={() => {setCountry("Washington")}}>USA</button>
+                <button className='btn btn-success' onClick={() => {setCountry("Warsaw")}}>Poland</button>
             </div>
             <hr />
             <p>{country}</p>
